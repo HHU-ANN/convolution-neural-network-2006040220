be0cfd99d6fc9bd200c1e8307359e6e77663fb34
= <=>
2023-06-27 15:00:12 +0800

    '3'

--- _03_Convolution_Neural_Network/Convolution_Neural_Network.py
+++ _03_Convolution_Neural_Network/Convolution_Neural_Network.py
@@ -15,13 +15,36 @@ from torch.utils.data import DataLoader
     
 
 class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super(MyAlexNet,self).__init__()
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(in_channels=3,out_channels=96,kernel_size=(3,3),stride=1,padding=1),nn.ReLU(),nn.MaxPool2d(kernel_size=(3,3),stride=2),
+            nn.Conv2d(in_channels=96,out_channels=256,kernel_size=(5,5),stride=1,padding=2),nn.ReLU(),nn.MaxPool2d(kernel_size=(3,3),stride=2),
+            nn.Conv2d(in_channels=256,out_channels=384,kernel_size=(3,3),padding=1),nn.ReLU(),
+            nn.Conv2d(in_channels=384,out_channels=384,kernel_size=(3,3),padding=1),nn.ReLU(),
+            nn.Conv2d(in_channels=384,out_channels=256,kernel_size=(3,3),padding=1),nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(3,3),stride=2)
+                                    )
+        self.layer2 = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(256*3*3, 1024), nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(1024, 1024), nn.ReLU(),
+            nn.Dropout(p=0.5),
+            nn.Linear(1024, 10)
+        )
+ 
+    def forward(self,x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        return x
     pass
 
 def read_data():
     # 这里可自行修改数据预处理，batch大小也可自行调整
     # 保持本地训练的数据读取和这里一致
-    dataset_train = torchvision.datasets.CIFAR10(root='../data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
-    dataset_val = torchvision.datasets.CIFAR10(root='../data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
+    dataset_train = torchvision.datasets.CIFAR10(root='data/exp03', train=True, download=True, transform=torchvision.transforms.ToTensor())
+    dataset_val = torchvision.datasets.CIFAR10(root='data/exp03', train=False, download=False, transform=torchvision.transforms.ToTensor())
     data_loader_train = DataLoader(dataset=dataset_train, batch_size=256, shuffle=True)
     data_loader_val = DataLoader(dataset=dataset_val, batch_size=256, shuffle=False)
     return dataset_train, dataset_val, data_loader_train, data_loader_val
